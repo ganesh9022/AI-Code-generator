6c0d88008ec59c775cc48893059cc15521095910
geetanjali-mali-pst <geetanjali.mali@positsource.com>
2024-12-27 13:28:11 +0530

    Added authentication for project

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,13 +4,22 @@ import { router } from "./Router";
 import { theme } from "./theme";
 import { RouterProvider } from "react-router-dom";
 import { ToolsProvider } from "./components/CodeCompletionToolsProviders";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 
 export default function App() {
   return (
-    <MantineProvider theme={theme}>
-      <ToolsProvider>
-        <RouterProvider router={router} />
-      </ToolsProvider>
-    </MantineProvider>
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+        <MantineProvider theme={theme}>
+          <ToolsProvider>
+            <RouterProvider router={router} />
+          </ToolsProvider>
+        </MantineProvider>
+      </SignedIn>
+    </header>
   );
 }
