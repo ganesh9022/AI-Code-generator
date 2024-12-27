592054f8231d186f11a38ba09c6a5a17a4c0c0ef
sachin-gajalwad-pst <sachin@positsource.com>
2024-12-27 08:55:15 +0530

    Added hook to fetch apis

--- backend/groqclould/groq_response.py
+++ backend/groqclould/groq_response.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-api_key = os.getenv('GROQ_API_KEY')
+api_key = os.getenv("GROQ_API_KEY")
 
 if not api_key:
     raise ValueError("API key for Groq is not set in the environment variables.")
@@ -15,32 +15,45 @@ if not api_key:
 client = Groq(api_key=api_key)
 MODEL = "llama3-70b-8192"
 
+
 def get_groq_response(prompt_content):
     try:
         response = client.chat.completions.create(
             model=MODEL,
             messages=[
                 {"role": "system", "content": SYSTEM_PROMPT},
-                {"role": "user", "content": prompt_content}
+                {"role": "user", "content": prompt_content},
             ],
             max_tokens=256,
-            temperature=0.5
+            temperature=0.5,
         )
         return response.choices[0].message.content
     except Exception as e:
         return str(e)
 
+
 def generate_code_suggestion(
     prompt: str,
     suffix: Optional[str] = None,
 ) -> dict:
+    print(f"prompt={prompt}, suffix={suffix}")
     if not prompt or not suffix:
-        return jsonify({"error": "Both 'prompt' and 'suffix' parameters are required."}), 400
+        return (
+            jsonify({"error": "Both 'prompt' and 'suffix' parameters are required."}),
+            400,
+        )
 
     input_prompt = f"{SYSTEM_PROMPT}\n\n**Code Context**:\nSurrounding Code:\n{prompt}\n\n### Current Line\n{suffix}"
     generated_code = get_groq_response(input_prompt)
 
-    return jsonify({"suggested_code": generated_code if generated_code else "No suggestion available."})
+    return jsonify(
+        {
+            "suggested_code": (
+                generated_code if generated_code else "No suggestion available."
+            )
+        }
+    )
+
 
 def answer_user_query(question: str) -> str:
     if not question:
--- front/src/pages/Editor/code_complitor.tsx
+++ front/src/pages/Editor/code_complitor.tsx
@@ -1,33 +1,78 @@
-import React from "react";
-import Editor, { OnMount } from "@monaco-editor/react";
-import * as monaco from "monaco-editor";
+import React, { useEffect } from "react";
+import Editor, { useMonaco } from "@monaco-editor/react";
 import { Paper, Title } from "@mantine/core";
 import { useTools } from "../../components/CodeCompletionToolsProviders";
+import { CompletionFormatter } from "../../components/completion-formatter";
+import useApi from "../../hooks/useApi";
 
 const CodeCompletionEditor: React.FC = () => {
-  const { language, editorRef, output, handleCodeChange } = useTools();
+  const { language, editorRef, output } = useTools();
+  const monaco = useMonaco();
+  
+  const { data, error, loading } = useApi("code-snippet", {
+    prompt: "def all_odd_numbers(a,b)",
+    suffix: "return result",
+  });
 
-  const handleEditorDidMount: OnMount = (editor) => {
-    editorRef.current = editor;
+  useEffect(() => {
+    if (!monaco) return;
 
-    editor.onKeyDown((e) => {
-      if ((e.ctrlKey || e.metaKey) && e.keyCode === monaco.KeyCode.Enter) {
-        e.preventDefault();
-        const currentValue = editor.getValue();
-        console.log("Editor content changed:", currentValue);
-        handleCodeChange(currentValue);
+    const provider = monaco.languages.registerInlineCompletionsProvider(
+      language,
+      {
+        provideInlineCompletions: async (model, position) => {
+          if (
+            !/[a-zA-Z0-9\s]/.test(model.getValue().charAt(position.column - 2))
+          ) {
+            return {
+              items: [],
+            };
+          }
+
+          return {
+            items: [
+              {
+                insertText: `console.log('Hello, world!');
+                kfdbklnf ldfbnfkj
+                fbkjfn fdkbnkj`,
+                range: {
+                  startLineNumber: position.lineNumber,
+                  startColumn: position.column - 1,
+                  endLineNumber: position.lineNumber,
+                  endColumn: position.column,
+                },
+              },
+            ].map((suggestion) =>
+              new CompletionFormatter(model, position).format(
+                suggestion.insertText,
+                suggestion.range
+              )
+            ),
+          };
+        },
+        freeInlineCompletions: () => {},
       }
-    });
-  };
+    );
+    return () => provider.dispose();
+  }, [monaco, language]);
 
   return (
     <div style={{ height: "90vh" }}>
       <Paper style={{ display: "flex", height: "100%" }}>
         <Paper w={output ? "50%" : "100%"}>
           <Editor
-            onMount={handleEditorDidMount}
+            onMount={(editor) => {
+              editorRef.current = editor;
+            }}
             theme="vs-dark"
             language={language}
+            options={{
+              autoClosingBrackets: "never",
+              autoClosingQuotes: "never",
+              formatOnType: true,
+              formatOnPaste: true,
+              trimAutoWhitespace: true,
+            }}
           />
         </Paper>
 
