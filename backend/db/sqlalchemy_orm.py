fb704aa30691f6c354b5444434539b7738a779b4
ritesh-jadhav-pst <ritesh.jadhav@positsource.com>
2024-12-30 13:08:46 +0530

    sqlalchemy setup

---
+++ backend/db/sqlalchemy_orm.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+db = create_engine('sqlite:///mydb.db', echo=True)
+
+Session = sessionmaker(bind=db)
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    email = Column(String)
+    def __repr__(self) -> str:
+        return f"<User(id={self.id}, username={self.username}, email={self.email})>"
+
+
+def main() -> None:
+    Base.metadata.create_all(db)
+    user = User(username="test", email="test@email.com")
+
+    with Session() as session:
+        session.add(user)
+        session.commit()
+        print(session.query(User).all())
+
+
+if __name__ == "__main__":
+    main()
