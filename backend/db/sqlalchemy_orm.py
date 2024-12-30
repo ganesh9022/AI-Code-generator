acd2ecac6e05b2657d6b0bdc2206d9ddeeef271e
ritesh-jadhav-pst <ritesh.jadhav@positsource.com>
2024-12-30 15:29:42 +0530

    updates

--- backend/db/sqlalchemy_orm.py
+++ backend/db/sqlalchemy_orm.py
@@ -5,7 +5,7 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL",echo=True)
 db = create_engine(DATABASE_URL)
 
 Session = sessionmaker(bind=db)
